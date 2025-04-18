b5cf1506fc1952f2e72a488f5149ac46a0171417
nichomueller <nicholas.mueller@monash.edu>
2025-04-18 12:44:41 +1000

    bugfix docs

--- src/RB/RBTransient/ReductionMethods.jl
+++ src/RB/RBTransient/ReductionMethods.jl
@@ -93,48 +93,35 @@ and a `θ`-method in time, one gets the space-time system
 where
 
 ```math
-\begin{equation}
-A_{\theta} =
-\begin{bmatrix}
-A_1 + M / (\theta \Delta t) & 0 & 0 & \hdots & 0 & 0 \\
-- M / (\theta \Delta t) & A_2 + M / (\theta \Delta t) & 0 & \hdots & 0 & 0 \\
-0 & - M / (\theta \Delta t) & A_3 + M / (\theta \Delta t) & & 0 & 0 \\
-& \ddots & \ddots & \ddots & & \\
-0 & 0 & 0 & \hdots & - M / (\theta \Delta t) & A_n + M / (\theta \Delta t)
+A_{\theta} = \begin{bmatrix}
+A_1 + M / (\theta \Delta t) & & & & & \\
+- M / (\theta \Delta t) & A_2 + M / (\theta \Delta t) & & & & \\
+& - M / (\theta \Delta t) & A_3 + M / (\theta \Delta t) & & & \\
+& & \ddots & \ddots & & \\
+& & & & - M / (\theta \Delta t) & A_n + M / (\theta \Delta t)
 \end{bmatrix};
-\end{equation}
 ```
 
 ```math
-\begin{equation}
-u_{\theta} = [(1-\theta)u_0 + \theta u_1, \hdots, (1-\theta)u_{n-1} + \theta u_n]^T;
-\end{equation}
+u_{\theta} = \left[(1-\theta)u_0 + \theta u_1, \hdots, (1-\theta)u_{n-1} + \theta u_n\right]^T;
 ```
 
 ```math
-\begin{equation}
-f_{\theta} = [f_1, \hdots, f_n]^T;
-\end{equation}
+f_{\theta} = \left[f_1, \hdots, f_n\right]^T;
 ```
 
 ```math
-\begin{equation}
 A_k = A(t_{k-1} + \theta \Delta t);
-\end{equation}
 ```
 
 ```math
-\begin{equation}
 f_k = f(t_{k-1} + \theta \Delta t).
-\end{equation}
 ```
 
 Note: instead of multiplying ``A_{\theta}`` by ``u_{\theta}``, we multiply ``\tilde{A}_{\theta}`` by ``u``, where
 
 ```math
-\begin{equation}
 \tilde{A}_{\theta} = tridiag((1-\theta)A_{k-1} - M / \Delta t, \theta A_k + M / \Delta t, 0).
-\end{equation}
 ```
 
 We now denote with ``\Phi`` and ``\Psi`` the spatial and temporal basis obtained by reducing the
@@ -142,33 +129,31 @@ snapshots associated to the state variable ``u``. The Galerkin projection of the
 space-time system is equal to ``\hat{A}_{\theta}\hat{u} = \hat{f}_{\theta}``, where ``\hat{u}`` is the unknown, and
 
 ```math
-\begin{equation}
+\begin{equation*}
 \begin{split}
 \hat{A}_{\theta} &= \sum\limits_{k=1}^{n-1} ( (1-θ) \Phi^T A_k \Phi - \Phi^T M \Phi / \Delta t) \otimes \Psi[k-1,:]^T \Psi[k,:]
   + \sum\limits_{k=1}^n (θ \Phi^T A_k \Phi + \Phi^T M \Phi / \Delta t) \otimes \Psi[k,:]^T \Psi[k,:] \\
   &= \theta A_{backwards} + (1-\theta)A_{forwards} + (M_{backwards} + M_{forwards}) / \Delta t \\
 \hat{f}_{\theta} &= \sum\limits_{k=1}^n \Phi^T f_k \otimes \Psi[k,:]
 \end{split}
-\end{equation}
+\end{equation*}
 ```
 
 We notice that the expression of ``\hat{A}_{\theta}`` can be written in a more general form as
 
 ```math
-\begin{equation}
 \hat{A}_{\theta} = combine_A(A_{backwards},A_{forwards}) + combine_M(M_{backwards},M_{forwards}),
-\end{equation}
 ```
 
 where combine_A and combine_M are two function specific to A and M:
 
 ```math
-\begin{equation}
+\begin{equation*}
 \begin{split}
 combine_A(x,y) &= \theta y + (1-\theta)y \\
 combine_M(x,y) &= (x - y) / \Delta t
 \end{split}
-\end{equation}
+\end{equation*}
 ```
 
 The same can be said of any time marching scheme. This is the meaning of the
