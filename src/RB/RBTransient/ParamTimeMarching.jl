55ca32ba880ef686f562771caa93e085759be16e
Nicholas Mueller <nicholas.mueller@monash.edu>
2026-04-12 15:17:17 +0200

    stashing

--- src/RB/RBTransient/ParamTimeMarching.jl
+++ src/RB/RBTransient/ParamTimeMarching.jl
@@ -1,13 +1,31 @@
+function Algebra.solve(
+  solver::ODESolver,
+  op::TransientRBOperator,
+  r::TransientRealisation,
+  uhs0::Tuple{Vararg{Function}}
+  )
+
+  trial = get_trial(op)
+  params = get_params(r)
+  us0 = ()
+  for uh0 in uhs0
+    u0 = get_free_dof_values(uh0(params))
+    û0 = project(trial,u0)
+    us0 = (us0...,reduced_vector(û0,u0))
+  end
+  solve(solver,op,r,us0)
+end
+
 function ODEs.ode_finish!(
   uf::RBParamVector,
   solver::ODESolver,
-  op::RBOperator,
+  op::TransientRBOperator,
   r::TransientRealisation,
-  statef::Tuple{Vararg{AbstractVector}},
+  statef::Tuple{Vararg{RBParamVector}},
   odecache
   )
 
-  copyto!(uf.fe_data,first(statef))
+  copyto!(uf,first(statef))
   inv_project(get_trial(op),uf)
   uf
 end
@@ -98,7 +116,6 @@ function ODEs._udate_theta!(
   x::RBParamVector
   )
   
-  println("HELLO")
   statef
 end
 
@@ -123,4 +140,37 @@ function ODEs._update_alpha2!(
   )
   
   statef
+end
+
+function ParamODEs.collect_param_solutions(sol::ODEParamSolution{<:RBParamVector{T,<:ConsecutiveParamVector{T}}}) where T
+  u0 = first(sol.us0)
+  ncols = num_params(sol.r)*num_times(sol.r)
+  sols = ParamODEs._allocate_solutions(u0,ncols)
+  for (k,(rk,uk)) in enumerate(sol)
+    ParamODEs._collect_solutions!(sols.data,uk.data,k)
+  end
+  trial = get_trial(sol.odeop)(sol.r)
+  inv_project!(sols.fe_data,trial,sols.data)
+  return sols
+end
+
+function ParamODEs.collect_param_solutions(sol::ODEParamSolution{<:RBParamVector{T,<:BlockParamVector{T}}}) where T
+  u0 = first(sol.us0)
+  u0item = testitem(u0)
+  ncols = num_params(sol.r)*num_times(sol.r)
+  sols = ParamODEs._allocate_solutions(u0,ncols)
+  for (k,(rk,uk)) in enumerate(sol)
+    for i in 1:blocklength(u0item)
+      ParamODEs._collect_solutions!(blocks(sols.data)[i],blocks(uk.data)[i],k)
+    end
+  end
+  trial = get_trial(sol.odeop)(sol.r)
+  inv_project!(sols.fe_data,trial,sols.data)
+  return sols
+end
+
+function ParamODEs._allocate_solutions(u0::RBParamVector,ncols) 
+  data = ParamODEs._allocate_solutions(u0.data,ncols)
+  fe_data = ParamODEs._allocate_solutions(u0.fe_data,ncols)
+  RBParamVector(data,fe_data)
 end
